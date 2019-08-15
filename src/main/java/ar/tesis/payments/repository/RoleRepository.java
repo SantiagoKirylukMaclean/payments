52ed419eebb8f814cb13587a4ed4fea3fe17b0b3
Kiryluk Maclean <p044125@ccba.usr.bpba>
2019-08-15 20:07:47 -0300

    first commit

---
+++ src/main/java/ar/tesis/payments/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package ar.tesis.payments.repository;
+
+
+import ar.tesis.payments.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
---
+++ src/main/java/ar/tesis/payments/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ar.tesis.payments.repository;
+
+
+import ar.tesis.payments.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
