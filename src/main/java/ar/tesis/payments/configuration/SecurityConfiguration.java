e364f3cbaa56f5bf96cb14e075658dc3d23adaf2
Kiryluk Maclean <p044125@ccba.usr.bpba>
2019-08-19 19:29:49 -0300

    Working

--- src/main/java/ar/tesis/payments/configuration/SecurityConfiguration.java
+++ src/main/java/ar/tesis/payments/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package ar.tesis.payments.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,6 +23,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
+
+    @Autowired
+    private CustomLoginSuccessHandler customLoginSuccessHandler;
 
     @Value("${spring.queries.users-query}")
     private String usersQuery;
@@ -51,7 +55,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
-                .defaultSuccessUrl("/admin/home")
+                .successHandler(customLoginSuccessHandler)
                 .usernameParameter("email")
                 .passwordParameter("password")
                 .and().logout()
@@ -66,5 +70,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .ignoring()
                 .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
     }
+
+
 
 }
--- src/main/java/ar/tesis/payments/configuration/WebMvcConfig.java
+++ src/main/java/ar/tesis/payments/configuration/WebMvcConfig.java
@@ -3,7 +3,13 @@ package ar.tesis.payments.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -13,5 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
+
+
 
 }
