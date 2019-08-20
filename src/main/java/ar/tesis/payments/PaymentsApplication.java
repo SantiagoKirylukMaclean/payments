242982d1823f9de7e3d46527585fc5bf6a5fe3ba
Kiryluk Maclean <p044125@ccba.usr.bpba>
2019-08-20 00:42:06 -0300

    Working whit loguer and botacora.

--- src/main/java/ar/tesis/payments/PaymentsApplication.java
+++ src/main/java/ar/tesis/payments/PaymentsApplication.java
@@ -1,13 +1,17 @@
 package ar.tesis.payments;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PaymentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentsApplication.class, args);
+		log.info("testing logging with lombok");
+		log.error("testing error with lombok");
 	}
 
 }
