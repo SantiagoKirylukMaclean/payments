1b6b5682801d2f5dda9d3110202c3fb979ef5422
Kiryluk Maclean <p044125@ccba.usr.bpba>
2019-08-19 22:53:01 -0300

    Final working MultiIdioma.

--- src/main/java/ar/tesis/payments/controller/LoginController.java
+++ src/main/java/ar/tesis/payments/controller/LoginController.java
@@ -64,6 +64,7 @@ public class LoginController {
         User user = userService.findUserByEmail(auth.getName());
         modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
         modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        modelAndView.addObject("user", user);
         modelAndView.setViewName("admin/home");
         return modelAndView;
     }
